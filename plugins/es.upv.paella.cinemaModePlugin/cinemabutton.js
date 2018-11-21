085ff0b1661c9ca69ccf86c382749e8c615c8749
androidwiltron <andrew.j.wilson7@googlemail.com>
2018-11-21 14:32:09 +0000

    addition of a cinema mode plugin to activate the cinemamode function on videolounge

---
+++ plugins/es.upv.paella.cinemaModePlugin/cinemabutton.js
@@ -0,0 +1,27 @@
+//paella.plugins.PlayPauseButtonPlugin = Class.create(paella.ButtonPlugin, {
+paella.addPlugin(function() {
+	return class CinemaModePlugin extends paella.ButtonPlugin {
+		constructor() {
+			super();
+			this.playIconClass = 'icon-airplay';
+			this.pauseIconClass = 'icon-airplay';
+			this.playSubclass = 'playButton';
+			this.pauseSubclass = 'pauseButton';
+		}
+
+		getAlignment() { return 'right'; }
+		getSubclass() { return this.playSubclass; }
+		getIconClass() { return this.playIconClass; }
+		getName() { return "es.upv.paella.cinemaModePlugin"; }
+		getDefaultToolTip() { return base.dictionary.translate("Cinema Mode"); }
+		getIndex() { return 110; }
+
+		checkEnabled(onSuccess) {
+			onSuccess(true);
+		}
+
+		action(button) {
+			parent.postMessage("cinemamode", "*");
+		}
+	}
+});
