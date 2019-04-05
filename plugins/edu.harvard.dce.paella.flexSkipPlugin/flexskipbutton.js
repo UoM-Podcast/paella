9a98f90d145c82cbbc3e91410678ab15d6621e39
Andrew Wilson <andrew.wilson-2@manchester.ac.uk>
2019-04-05 15:47:21 +0100

    VL-316 improve playback bar ux including font size colurs and playback x2 speed

--- plugins/edu.harvard.dce.paella.flexSkipPlugin/flexskipbutton.js
+++ plugins/edu.harvard.dce.paella.flexSkipPlugin/flexskipbutton.js
@@ -4,15 +4,15 @@ paella.addPlugin(function() {
 		getName() { return "edu.harvard.dce.paella.flexSkipPlugin"; }
 		getIndex() { return 121; }
 		getSubclass() { return 'flexSkip_Rewind_10'; }
-		getIconClass() { return 'icon-back-10-s'; }
+		getIconClass() { return 'icon-backward2'; }
 		formatMessage() { return 'Rewind 10 seconds'; }
 		getDefaultToolTip() { return base.dictionary.translate(this.formatMessage()); }
 		getMinWindowSize() { return 510; }
-	
+
 		checkEnabled(onSuccess) {
 			onSuccess(!paella.player.isLiveStream());
 		}
-		
+
 		action(button) {
 			paella.player.videoContainer.currentTime()
 				.then(function(currentTime) {
@@ -32,9 +32,9 @@ paella.addPlugin(function() {
 		getIndex() { return 122; }
 		getName() { return "edu.harvard.dce.paella.flexSkipForwardPlugin"; }
 		getSubclass() { return 'flexSkip_Forward_30'; }
-		getIconClass() { return 'icon-forward-30-s'; }
+		getIconClass() { return 'icon-forward3'; }
 		formatMessage() { return 'Forward 30 seconds'; }
-		
+
 		action(button) {
 			paella.player.videoContainer.currentTime()
 				.then(function(currentTime) {
